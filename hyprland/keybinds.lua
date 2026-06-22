a51751a5f000dd8d31e0b4a23b20e2e8bd62ac5b
lily <lily@transgirls.love>
2026-06-22 03:22:35 -0700

    Simplify keybinds

--- hyprland/keybinds.lua
+++ hyprland/keybinds.lua
@@ -1,39 +1,22 @@
 local MAIN_MOD = "SUPER"
-local CAPS_LOCK = "code:66"
 
--- SUPER RETURN        ~ Launch Terminal
--- SUPER L             ~ Lock Screen
--- SUPER M             ~ Logout
--- SUPER E             ~ File Manager
--- SUPER SHIFT S       ~ Screenshot
--- CTRL + ALT + Delete ~ Logout
--- CTRL + SPACE        ~ App Launcher
--- XF86AudioPlay       ~ Media Play
--- CAPS_LOCK           ~ Launch Terminal
--- SUPER G             ~ Browser
--- SUPER D             ~ Discord
--- SUPER Y             ~ YouTube Music
--- SUPER Page_Up       ~ Zoom In
--- SUPER Page_Down     ~ Zoom Out
 -- SUPER Q             ~ Launch Terminal
+-- SUPER E             ~ File Manager
+-- SUPER SHIFT S       ~ Screenshot (copy to clipboard)
+-- CTRL + SPACE        ~ App Launcher
+-- SUPER B             ~ Browser
+-- SUPER D             ~ Discord
+-- SUPER M             ~ Music
 -- SUPER C             ~ Close Focused Window
 
 local BINDS = {
-    TERMINAL        = {MAIN_MOD .. " + RETURN",       "kitty"},
-    LOCK            = {MAIN_MOD .. " + L",             "hyprlock"},
-    LOGOUT          = {MAIN_MOD .. " + M",             "wlogout --protocol layer-shell"},
+    TERMINAL        = {MAIN_MOD .. " + Q",             "kitty"},
     FILE_MANAGER    = {MAIN_MOD .. " + E",             "dolphin"},
-    SCREENSHOT      = {MAIN_MOD .. " + SHIFT + S",     'grim -g "$(slurp)" - | swappy -f -'},
-    LOGOUT_CTRL     = {"CTRL + ALT + Delete",          "wlogout"},
+    SCREENSHOT      = {MAIN_MOD .. " + SHIFT + S",     'grim -g "$(slurp)" - | wl-copy'},
     APP_LAUNCHER    = {"CTRL + SPACE",                 "~/.scripts/game_safe_app_launcher.sh -- ulauncher-toggle"},
-    MEDIA_PLAY      = {"XF86AudioPlay",                "playerctl play-pause"},
-    CAPS_TERMINAL   = {CAPS_LOCK,                      "kitty"},
-    BROWSER         = {MAIN_MOD .. " + G",             "zen-browser"},
+    BROWSER         = {MAIN_MOD .. " + B",             "zen-browser"},
     DISCORD         = {MAIN_MOD .. " + D",             "~/.scripts/start_vesktop.sh"},
-    YOUTUBE_MUSIC   = {MAIN_MOD .. " + Y",             "~/Music/YouTube-Music-3.11.0.AppImage"},
-    ZOOM_IN         = {MAIN_MOD .. " + Page_Up",       "~/.config/hypr/hyprzoom -duration 1 -steps 5 -target 3"},
-    ZOOM_OUT        = {MAIN_MOD .. " + Page_Down",     "~/.config/hypr/hyprzoom -duration 1 -steps 5 -target 3"},
-    TERMINAL_Q      = {MAIN_MOD .. " + Q",             "kitty"},
+    MUSIC           = {MAIN_MOD .. " + M",             "~/Music/YouTube-Music-3.11.0.AppImag"},
 }
 
 for _, bind in pairs(BINDS) do
