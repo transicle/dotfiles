c3b9a984d9606d3f28bf05a35299d5996f7520eb
lily <lily@transgirls.love>
2026-06-22 05:54:02 -0700

    upd: lazily commit on June 22nd, 2026 5:54 AM UTC -0700 by lily

--- scripts/downloaders/download-hyprpaper.sh
+++ scripts/downloaders/download-hyprpaper.sh
@@ -3,6 +3,16 @@ set -e
 
 HYPRPAPER="$HOME/.config/hyprpaper"
 
+if [[ -d "$HYPRPAPER" ]]; then
+    n=0
+    DEST="$HOME/.config/hyprpaper.old"
+    while [[ -d "$DEST" ]]; do
+        n=$((n + 1))
+        DEST="$HOME/.config/hyprpaper-$n.old"
+    done
+    mv "$HYPRPAPER" "$DEST"
+fi
+
 TMPDIR=$(mktemp -d)
 git clone --depth=1 https://github.com/transicle/dotfiles "$TMPDIR/dotfiles"
 mkdir -p "$HYPRPAPER"
--- scripts/downloaders/download-kitty.sh
+++ scripts/downloaders/download-kitty.sh
@@ -3,6 +3,16 @@ set -e
 
 KITTY="$HOME/.config/kitty"
 
+if [[ -d "$KITTY" ]]; then
+    n=0
+    DEST="$HOME/.config/kitty.old"
+    while [[ -d "$DEST" ]]; do
+        n=$((n + 1))
+        DEST="$HOME/.config/kitty-$n.old"
+    done
+    mv "$KITTY" "$DEST"
+fi
+
 TMPDIR=$(mktemp -d)
 git clone --depth=1 https://github.com/transicle/dotfiles "$TMPDIR/dotfiles"
 mkdir -p "$KITTY"
