aa8490228ba903f5e004e5d84173b1bf622a040c
lily ❤︎⁠ <lily@transgirls.love>
2026-08-01 04:52:21 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -7,6 +7,7 @@ bash <(curl -s "$REPO/scripts/dependencies.sh")
 bash <(curl -s "$REPO/scripts/downloaders/download-hypr.sh")
 bash <(curl -s "$REPO/scripts/downloaders/download-hyprpaper.sh")
 bash <(curl -s "$REPO/scripts/downloaders/download-kitty.sh")
+bash <(curl -s "$REPO/scripts/downloaders/download-waybar.sh")
 
 bash <(curl -s "$REPO/scripts/config/config-equicord.sh")
 bash <(curl -s "$REPO/scripts/config/config-files.sh")
