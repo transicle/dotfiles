12f713fa8e93661419414070adfa43ddd49fe717
lily ❤︎⁠ <lily@transgirls.love>
2026-08-01 04:59:01 -0700

    Update config-equicord.sh

--- scripts/config/config-equicord.sh
+++ scripts/config/config-equicord.sh
@@ -6,10 +6,8 @@ TMPDIR=$(mktemp -d)
 git clone --depth=1 https://github.com/transicle/dotfiles "$TMPDIR/dotfiles"
 
 DEST="$HOME/.var/app/org.equicord.equibop/config/equibop/settings"
-mkdir -p "$DEST"
 
 cp -f "$TMPDIR/dotfiles/equibop/settings.json" "$DEST/settings.json"
-
 rm -rf "$TMPDIR"
 
 echo "Installed to $DEST/settings.json"
