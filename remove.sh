bfd7788d440f559212f24df7f97565fd30450cbc
lily ❤︎⁠ <lily@transgirls.love>
2026-08-15 09:38:57 -0700

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -6,4 +6,4 @@ REPO="https://raw.githubusercontent.com/transicle/dotfiles/refs/heads/main"
 bash <(curl -s "$REPO/scripts/removers/remove-hyprpaper.sh")
 bash <(curl -s "$REPO/scripts/removers/remove-hypr.sh")
 bash <(curl -s "$REPO/scripts/removers/remove-kitty.sh")
-
+bash <(curl -s "$REPO/scripts/removers/remove-waybar.sh")
