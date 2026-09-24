2c1ef11d18ac964f4a43cdf006f35460f8f6f327
lily ❤︎⁠ <lily@transgirls.love>
2026-09-24 09:15:17 -0700

    Update keybinds.lua

--- hypr/keybinds.lua
+++ hypr/keybinds.lua
@@ -4,7 +4,7 @@ local BINDS = {
     -- Apps
     {MAIN_MOD .. " + Q",          hl.dsp.exec_cmd("kitty")},
     {MAIN_MOD .. " + E",          hl.dsp.exec_cmd("dolphin")},
-    {MAIN_MOD .. " + B",          hl.dsp.exec_cmd("~/.tarball-instructions/zen/zen")},
+    {MAIN_MOD .. " + B",          hl.dsp.exec_cmd("~/.tarball-installations/zen/zen")},
     {MAIN_MOD .. " + D",          hl.dsp.exec_cmd("flatpak run org.equicord.equibop")},
     {MAIN_MOD .. " + I",          hl.dsp.exec_cmd("code")},
     {MAIN_MOD .. " + L",          hl.dsp.exec_cmd("ulauncher-toggle --hide-window --no-window-shadow")},
